ed406818e77a1b4dc4db82213b80c17123f148e0
Jamie Wu <jamiewu2@illinois.edu>
2022-10-03 20:29:00 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  nixConfig.extra-substituters = "https://cache.garnix.io";
+  nixConfig.extra-trusted-public-keys = "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
+
+  inputs = {
+    emanote.url = "github:EmaApps/emanote";
+    nixpkgs.follows = "emanote/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.emanote.flakeModule
+      ];
+      perSystem = { self', pkgs, system, ... }: {
+        emanote = {
+          # By default, the 'emanote' flake input is used.
+          # package = inputs.emanote.packages.${system}.default;
+          sites."default" = {
+            path = ./content;
+            pathString = "./content";
+            # port = 8080;
+            baseUrl = "/emanote-template/";  # Change to "/" (or remove it entirely) if using CNAME
+            # prettyUrls = true;
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nixpkgs-fmt
+            pkgs.zk
+          ];
+        };
+      };
+    };
+}
